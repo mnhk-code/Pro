9f0e0e59acc2042ec0157a381071c17b32f7344c
menna khalid <mnhk32134@gmail.com>
2026-07-29 12:20:11 +0300

    Update Menu.js

--- assets/js/Menu.js
+++ assets/js/Menu.js
@@ -1,27 +1,26 @@
 const prices = document.querySelectorAll(".price");
-const section = document.querySelector(".prices");
+const section = document.querySelector("main");
 
 let started = false;
 
 window.onscroll = function () {
   if (window.scrollY >= section.offsetTop - 300) {
     if (!started) {
-      prices.forEach((price) => startCount(price));
+      prices.forEach((price) => {
+        let start = 0;
+        let end = +price.dataset.price;
+
+        let counter = setInterval(() => {
+          start++;
+          price.textContent = "$" + start;
+
+          if (start >= end) {
+            clearInterval(counter);
+          }
+        }, 2000 / end);
+      });
+
       started = true;
     }
   }
 };
-
-function startCount(price) {
-  let start = 0;
-  let end = +price.dataset.price;
-
-  let counter = setInterval(() => {
-    start++;
-    price.textContent = "$" + start;
-
-    if (start >= end) {
-      clearInterval(counter);
-    }
-  }, 2000 / end);
-}
